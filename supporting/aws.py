6e0f35db848a3052979ea58013bbaf1f9fb070fa
Peter van Elsacker <p.van.elsacker@om.nl>
2025-04-14 16:10:42 +0200

    fixed

---
+++ supporting/aws.py
@@ -0,0 +1,76 @@
+import boto3
+from boto3.dynamodb.conditions import Attr
+from botocore.exceptions import ClientError
+
+def dynamodb_query(table, filter=None):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table)
+
+    # Scan the table with a filter to get items where 'check' is True
+    response = table.scan(
+        FilterExpression=Attr('check').eq(True)
+    )
+
+    # Retrieve and print the items
+    items = response.get('Items', [])
+    return items
+
+
+def all_events(table, filter=None):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table)
+
+    # Scan the table with a filter to get items where 'check' is True
+    response = table.scan()
+
+    # Retrieve and print the items
+    items = response.get('Items', [])
+    return items
+
+
+
+def dynamo_db_update(table, item_id='', attribute='', value=''):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('events')
+    try:
+        # Update the item with id = '123', setting the value attribute
+        response = table.update_item(
+            Key={
+                'id': f'{item_id}'  # Primary key of the item to update
+            },
+            UpdateExpression="SET #v = :new_value",
+            ExpressionAttributeNames={
+                "#v": f"{attribute}"  # 'value' is the attribute to update
+            },
+            ExpressionAttributeValues={
+                ":new_value": value  # New value to set
+            }
+        )
+        return "ok"
+    except ClientError as e:
+        return e.response['Error']['Message']
+    except Exception as e:
+        return str(e)
+
+
+def put_item_dynamodb(table_name, item):
+    """
+    Inserts an item into a DynamoDB table.
+
+    Args:
+        table_name (str): The name of the DynamoDB table.
+        item (dict): The item to insert.
+
+    Returns:
+        dict: The response from the DynamoDB put_item operation, or None on failure.
+    """
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(table_name)
+
+        response = table.put_item(Item=item)
+        return response
+
+    except Exception as e:
+        print(f"Error putting item into DynamoDB: {e}")
+        return None
